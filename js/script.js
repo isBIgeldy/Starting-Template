27d003e271c384ec952ad57c917cc2c43e80e768
isBIgeldy <isprogrammer10@gmail.com>
2022-09-25 23:13:17 +0500

    add files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+   // burger 
+      $('.icon-menu').click(function(event) {
+         $('.icon-menu, .menu__body').toggleClass('_active');
+      });
+   // /burger
+});
